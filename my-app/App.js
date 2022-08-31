fd58ae47e5c65a66c517d52a14c4a81ea8486b87
kaizen <74340451+animaxlibera@users.noreply.github.com>
2022-08-30 17:02:41 -0700

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react'
-import { Button } from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Homescreen from "./source/screens/Home";
+import Detailscreen from "./source/screens/Details";
 
-const App = () => {
-   const handlePress = () => false
-   return (
-      <Button
-         onPress = {handlePress}
-         title = "Red button!"
-         color = "red"
-      />
-   )
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-export default App
