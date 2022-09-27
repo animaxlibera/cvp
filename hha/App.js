7ab80fa4e4dc360c604750ae5a6744930575fd93
Azaa12 <doogii1145@gmail.com>
2022-09-27 16:15:56 +0800

    zxc

--- hha/App.js
+++ hha/App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appcontainer}>
+      <View style={styles.inputcontainer}>
+         <TextInput style={styles.TextInput} placeholder='your goals' />
+         <Button title=" add goal"/>
+      </View>
+      <View>
+        <Text>list of goals. . .</Text>  
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ appcontainer: {
+    padding: 50,
+ },
+ inputcontainer: {
+   flexDirection: 'row',
+   justifyContent: 'space-between '
+ },
+ TextInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '71%' 
+  
+ }
 });
