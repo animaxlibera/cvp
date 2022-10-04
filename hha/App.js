37241f107eaa1fd8c1b88130e5d6568530bfd1ea
erdenne <erdenebateba26@gmail.com>
2022-10-04 14:56:12 +0800

    c

--- hha/App.js
+++ hha/App.js
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoaltext, setEnteredGoalText] = useState('');
+  const [courseGoals, setcourseGoals] = useState([]);
+  function goalinputHandler(enteredText){
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalhandler() {
+    setcourseGoals(currenCourseGoals => [...courseGoals, enteredGoaltext,]);
+  };
+
   return (
     <View style={styles.appcontainer}>
       <View style={styles.inputcontainer}>
-         <TextInput style={styles.TextInput} placeholder='your goals' />
-         <Button title=" add goal"/>
+         <TextInput style={styles.TextInput} placeholder='your goals'
+          onChangeText={goalinputHandler}/>
+         <Button title=" add goal" onPress={addGoalhandler}/>
       </View>
-      <View>
-        <Text>list of goals. . .</Text>  
+      <View style={styles.goalscontainer}>
+            {courseGoals.map((goal) => <Text style={styles.goalitem} key={goal}>{goal}</Text> )}
       </View>
     </View>
   );
@@ -17,16 +29,35 @@ export default function App() {
 
 const styles = StyleSheet.create({
  appcontainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16
  },
  inputcontainer: {
+   flex: 1,
    flexDirection: 'row',
-   justifyContent: 'space-between '
+   justifyContent: 'space-between',
+   alignitems: 'center',
+   paddingBottom: 24,
+   borderBottomWidth: 1,
+   borderBottomColor: '#cccccc'
+
  },
  TextInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '71%' 
-  
+    width: '71%', 
+    marginRight: 8,
+    padding: 8 
+ },
+ goalscontainer:{
+  flex: 5
+ },
+ goalitem: {
+   margin: 8,
+   padding: 8,
+   borderRadius: 6,
+   backgroundColor: '#fa0000',
+   color: 'white'
  }
 });
